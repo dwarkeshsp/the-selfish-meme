46b735899dcc9842007ce727f20b796225e5731b
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2019-11-20 10:48:43 -0600

    newest education

--- src/routes/posts/2019-11-10-education-perpetuates-static-memes/post.js
+++ src/routes/posts/2019-11-10-education-perpetuates-static-memes/post.js
@@ -2,6 +2,6 @@ export default {
   title: "The Educational System is Designed by Anti-Rational Memes",
   tags: ["Memetics"],
   spoiler:
-    "This ghoulish machinery of indoctrination evolved under the tutelage and for the benefit of anti-rational memes, which carried it forward through the centuries because of the tremendous advantage it afforded them in generating a servile, credulous, groveling, and conformist population through which it could easily spread.",
+    "This ghoulish machinery of indoctrination evolved under the tutelage and for the benefit of anti-rational memes; the latter carried the former forward through the centuries because the educational system generates the servile, credulous, groveling, and conformist population which is kindling for mind viruses",
   getContent: () => import("./document.md")
 };
