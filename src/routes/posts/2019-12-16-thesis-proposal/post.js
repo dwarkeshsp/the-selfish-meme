fcef1948fc6dd02ef7a291dac2f2251e0fe96353
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2019-12-16 08:05:51 -0600

    thesis proposal

---
+++ src/routes/posts/2019-12-16-thesis-proposal/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Memetics Thesis Proposal for Polymathic Scholars",
+  tags: ["Memetics"],
+  spoiler: "Having seen the sturdy skeleton of this beautiful new theory, I am obsessed with filling in the necessary musculature.",
+  getContent: () => import("./document.md")
+};
